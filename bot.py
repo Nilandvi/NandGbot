73d4627d7fae61434f9b253f0876852adf903ec1
Nilandvi <Nilandvi@gmail.com>
2023-04-29 21:51:09 +0300

    fix

--- bot.py
+++ bot.py
@@ -12,15 +12,6 @@ wikipedia.set_lang("ru")
 bot = telebot.TeleBot(TOKEN)
 toggle = 1
 
-def webAppKeyboard(): #создание клавиатуры с webapp кнопкой
-   keyboard = types.ReplyKeyboardMarkup(row_width=1) #создаем клавиатуру
-   webAppTest = types.WebAppInfo("https://telegram.mihailgok.ru") #создаем webappinfo - формат хранения url
-   one_butt = types.KeyboardButton(text="Тестовая страница", web_app=webAppTest) #создаем кнопку типа webapp
-   keyboard.add(one_butt) #добавляем кнопки в клавиатуру
-
-   return keyboard #возвращаем клавиатуру
-
-
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     with Session() as session:
